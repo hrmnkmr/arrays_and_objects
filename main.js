a7f3e79b0250f8e77e4b536b2b8e5a44cf47987c
nakamura <hrm.nkmr04@gmail.com>
2024-07-29 08:04:57 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+var number = ["1", "2", "3", "4"];
+console.log(number);
+console.log("数値の代入");
+var number = ["1", "2", "3", "4"];
+console.log(number[0]);
+console.log(number[2]);
+console.log("1番目と3番目を表示");
+var feeling = {
+	name: "二郎",
+	age: 35,
+	condition: "良好",
+}
+console.log(feeling.condition);
+var weather = [{
+	month: "7月",
+	day: 3,
+	forecast: "晴れ",
+}, {
+	month: "7月",
+	day: 4,
+	forecast: "曇り",
+}, {
+	month: "7月",
+	day: 5,
+	forecast: "雨",
+}]
+console.log({
+	weather
+});
+console.log({
+	month: "7月"
+}, {
+	day: "3"
+}, {
+	forecast: "晴れ"
+});
+console.log({
+	weather
+});
+console.log({
+	month: "7月"
+}, {
+	day: "4"
+}, {
+	forecast: "曇り"
+});
+console.log({
+	weather
+});
+console.log({
+	month: "7月"
+}, {
+	day: "5"
+}, {
+	forecast: "雨"
+});
